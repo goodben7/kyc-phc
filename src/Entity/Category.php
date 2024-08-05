5ef007311f773b739df54d671001c39b40448eb2
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-08-05 16:27:51 +0100

    [Config] add Get and GetCollection in Category Entity

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,11 +2,40 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Get;
 use App\Doctrine\IdGenerator;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CategoryRepository;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_CODE', fields: ['code'])]
+#[ApiResource(
+    normalizationContext: ['groups' => 'category:get'],
+    operations: [
+        new Get(
+            security: 'is_granted("ROLE_CATEGORY_DETAILS")',
+            provider: ItemProvider::class
+        ),
+        new GetCollection(
+            security: 'is_granted("ROLE_CATEGORY_LIST")',
+            provider: CollectionProvider::class
+        )
+    ]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'id' => 'exact',
+    'label' => 'ipartial',
+    'description' => 'ipartial',
+    'code' => 'exact',
+    'actived' => 'exact'
+])]
 class Category
 {
     const ID_PREFIX = "CA";
@@ -15,19 +44,24 @@ class Category
     #[ORM\GeneratedValue( strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(IdGenerator::class)]
     #[ORM\Column(length: 16)]
+    #[Groups(groups: ['category:get'])]
     private ?string $id = null;
 
     #[ORM\Column(length: 120)]
+    #[Groups(groups: ['category:get'])]
     private ?string $label = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(groups: ['category:get'])]
     private ?string $description = null;
 
     #[ORM\Column(length: 30, nullable: true)]
+    #[Groups(groups: ['category:get'])]
     private ?string $code = null;
 
     #[ORM\Column]
-    private ?bool $actived = null;
+    #[Groups(groups: ['category:get'])]
+    private ?bool $actived = false;
 
     public function getId(): ?string
     {
