e3cd6ad0c64b1dd3558e56b22e55b8291306e204
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-08-05 16:15:01 +0100

    [Config] add Category Entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Doctrine\IdGenerator;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategoryRepository;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    const ID_PREFIX = "CA";
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue( strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(IdGenerator::class)]
+    #[ORM\Column(length: 16)]
+    private ?string $id = null;
+
+    #[ORM\Column(length: 120)]
+    private ?string $label = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 30, nullable: true)]
+    private ?string $code = null;
+
+    #[ORM\Column]
+    private ?bool $actived = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function isActived(): ?bool
+    {
+        return $this->actived;
+    }
+
+    public function setActived(bool $actived): static
+    {
+        $this->actived = $actived;
+
+        return $this;
+    }
+}
