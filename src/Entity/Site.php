8ea6e976395056041a49495c3102545291fd917b
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-08-05 14:52:57 +0100

    [Config] add site entity and GetCollection, Get

---
+++ src/Entity/Site.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\Get;
+use App\Doctrine\IdGenerator;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\SiteRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+
+#[ORM\Entity(repositoryClass: SiteRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_CODE', fields: ['code'])]
+#[ApiResource(
+    normalizationContext: ['groups' => 'site:get'],
+    operations: [
+        new Get(
+            security: 'is_granted("ROLE_SITE_DETAILS")',
+            provider: ItemProvider::class
+        ),
+        new GetCollection(
+            security: 'is_granted("ROLE_SITE_LIST")',
+            provider: CollectionProvider::class
+        )
+    ]
+)]
+class Site
+{
+    const ID_PREFIX = "SI";
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue( strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(IdGenerator::class)]
+    #[ORM\Column(length: 16)]
+    #[Groups(groups: ['site:get'])]
+    private ?string $id = null;
+
+    #[ORM\Column(length: 120)]
+    #[Groups(groups: ['site:get'])]
+    private ?string $label = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(groups: ['site:get'])]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 30, nullable: true)]
+    #[Groups(groups: ['site:get'])]
+    private ?string $code = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+}
