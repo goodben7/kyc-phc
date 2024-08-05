3aff88c85edacb179b19a588e35e4b26f26de6b5
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-08-05 17:17:57 +0100

    [Config] add Post and Patch Route in Function Entity

--- src/Entity/FunctionTitle.php
+++ src/Entity/FunctionTitle.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\Get;
 use App\Doctrine\IdGenerator;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Patch;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
@@ -11,7 +13,10 @@ use ApiPlatform\Metadata\GetCollection;
 use App\Repository\FunctionTitleRepository;
 use ApiPlatform\Doctrine\Orm\State\ItemProvider;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
 
 #[ORM\Entity(repositoryClass: FunctionTitleRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_CODE', fields: ['code'])]
@@ -25,7 +30,17 @@ use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
         new GetCollection(
             security: 'is_granted("ROLE_FUNCTION_LIST")',
             provider: CollectionProvider::class
-        )
+        ),
+        new Post(
+            security: 'is_granted("ROLE_FUNCTION_CREATE")',
+            denormalizationContext: ['groups' => 'function:post'],
+            processor: PersistProcessor::class,
+        ),
+        new Patch(
+            security: 'is_granted("ROLE_FUNCTION_UPDATE")',
+            denormalizationContext: ['groups' => 'function:patch'],
+            processor: PersistProcessor::class,
+        ),
     ]
 )]
 #[ApiFilter(SearchFilter::class, properties: [
@@ -42,20 +57,27 @@ class FunctionTitle
     #[ORM\Id]
     #[ORM\GeneratedValue( strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(IdGenerator::class)]
+    #[Groups(groups: ['function:get'])]
     #[ORM\Column(length: 16)]
     private ?string $id = null;
 
     #[ORM\Column(length: 120)]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Groups(groups: ['function:get', 'function:post', 'function:patch'])]
     private ?string $label = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(groups: ['function:get', 'function:post', 'function:patch'])]
     private ?string $description = null;
 
     #[ORM\Column(length: 30, nullable: true)]
+    #[Groups(groups: ['function:get', 'function:post', 'function:patch'])]
     private ?string $code = null;
 
     #[ORM\Column]
-    private ?bool $actived = null;
+    #[Groups(groups: ['function:get', 'function:post', 'function:patch'])]
+    private ?bool $actived = true;
 
     public function getId(): ?string
     {
