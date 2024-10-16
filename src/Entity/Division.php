446307cc63cfd8950a4f5e3be55a6f250284917f
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-10-16 11:26:29 +0100

    [Agent] add Division entity

---
+++ src/Entity/Division.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Doctrine\IdGenerator;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\DivisionRepository;
+
+#[ORM\Entity(repositoryClass: DivisionRepository::class)]
+class Division
+{
+    const ID_PREFIX = "DI";
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue( strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(IdGenerator::class)]
+    #[ORM\Column(length: 16)]    
+    private ?string $id = null;
+
+    #[ORM\Column(length: 120)]
+    private ?string $label = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $code = null;
+
+    #[ORM\Column]
+    private ?bool $actived = null;
+
+    #[ORM\ManyToOne(inversedBy: 'divisions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Site $site = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function isActived(): ?bool
+    {
+        return $this->actived;
+    }
+
+    public function setActived(bool $actived): static
+    {
+        $this->actived = $actived;
+
+        return $this;
+    }
+
+    public function getSite(): ?Site
+    {
+        return $this->site;
+    }
+
+    public function setSite(?Site $site): static
+    {
+        $this->site = $site;
+
+        return $this;
+    }
+}
--- src/Entity/Site.php
+++ src/Entity/Site.php
@@ -91,9 +91,16 @@ class Site
     #[Groups(groups: ['site:get', 'site:post', 'site:patch'])]
     private ?string $rate = null;
 
+    /**
+     * @var Collection<int, Division>
+     */
+    #[ORM\OneToMany(targetEntity: Division::class, mappedBy: 'site')]
+    private Collection $divisions;
+
     public function __construct()
     {
         $this->agents = new ArrayCollection();
+        $this->divisions = new ArrayCollection();
     }
 
     public function getId(): ?string
@@ -192,6 +199,36 @@ class Site
     public function setRate(?string $rate): static
     {
         $this->rate = $rate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Division>
+     */
+    public function getDivisions(): Collection
+    {
+        return $this->divisions;
+    }
+
+    public function addDivision(Division $division): static
+    {
+        if (!$this->divisions->contains($division)) {
+            $this->divisions->add($division);
+            $division->setSite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDivision(Division $division): static
+    {
+        if ($this->divisions->removeElement($division)) {
+            // set the owning side to null (unless already changed)
+            if ($division->getSite() === $this) {
+                $division->setSite(null);
+            }
+        }
 
         return $this;
     }
