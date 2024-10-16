606af0d4803e804225706c1cc7ae921c0fcfe70a
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-10-16 12:24:13 +0100

    [Agent] add Get, GetCollection, Post and Patch routes in Division entity and generate migration

--- src/Entity/Division.php
+++ src/Entity/Division.php
@@ -2,11 +2,44 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Get;
 use App\Doctrine\IdGenerator;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Patch;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DivisionRepository;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
 
 #[ORM\Entity(repositoryClass: DivisionRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_CODE', fields: ['code'])]
+#[ApiResource(
+    normalizationContext: ['groups' => 'division:get'],
+    operations: [
+        new Get(
+            security: 'is_granted("ROLE_DIVISION_DETAILS")',
+            provider: ItemProvider::class
+        ),
+        new GetCollection(
+            security: 'is_granted("ROLE_DIVISION_LIST")',
+            provider: CollectionProvider::class
+        ),
+        new Post(
+            security: 'is_granted("ROLE_DIVISION_CREATE")',
+            denormalizationContext: ['groups' => 'division:post'],
+            processor: PersistProcessor::class,
+        ),
+        new Patch(
+            security: 'is_granted("ROLE_DIVISION_UPDATE")',
+            denormalizationContext: ['groups' => 'division:patch'],
+            processor: PersistProcessor::class,
+        ),
+    ]
+)]
 class Division
 {
     const ID_PREFIX = "DI";
@@ -14,23 +47,29 @@ class Division
     #[ORM\Id]
     #[ORM\GeneratedValue( strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(IdGenerator::class)]
-    #[ORM\Column(length: 16)]    
+    #[ORM\Column(length: 16)]  
+    #[Groups(groups: ['division:get', 'agent:get'])]  
     private ?string $id = null;
 
     #[ORM\Column(length: 120)]
+    #[Groups(groups: ['division:get', 'division:post', 'division:patch', 'agent:get'])]
     private ?string $label = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(groups: ['division:get', 'division:post', 'division:patch'])]
     private ?string $description = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(groups: ['division:get', 'division:post', 'division:patch'])]
     private ?string $code = null;
 
     #[ORM\Column]
-    private ?bool $actived = null;
+    #[Groups(groups: ['division:get', 'division:post', 'division:patch'])]
+    private ?bool $actived = true;
 
     #[ORM\ManyToOne(inversedBy: 'divisions')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(groups: ['division:get', 'division:post', 'division:patch'])]
     private ?Site $site = null;
 
     public function getId(): ?string
